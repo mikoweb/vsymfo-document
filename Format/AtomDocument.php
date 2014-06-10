ffdfd884ea210f9ee2df50c606514813736274d1
Rafał Mikołajun <rafal@mikoweb.pl>
2014-06-10 17:01:50 +0000

    utworzono nowe typy dokumentów

---
+++ Format/AtomDocument.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the vSymfo package.
+ *
+ * website: www.vision-web.pl
+ * (c) Rafał Mikołajun <rafal@vision-web.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace vSymfo\Component\Document\Format;
+
+/**
+ * Dokument Atom
+ * @author Rafał Mikołajun <rafal@vision-web.pl>
+ * @package vSymfo Component
+ * @subpackage Document_Type
+ */
+class AtomDocument extends XmlDocument
+{
+    
+}
