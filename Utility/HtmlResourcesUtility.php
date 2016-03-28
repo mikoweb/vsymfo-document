23678760f131438220a7503bd8ce7670fc55b8be
Rafał Mikołajun <rafal@mikoweb.pl>
2016-03-28 19:42:52 +0200

    scss w HtmlResourcesUtility

--- Utility/HtmlResourcesUtility.php
+++ Utility/HtmlResourcesUtility.php
@@ -56,12 +56,14 @@ class HtmlResourcesUtility
     protected function setDefaultOptions(OptionsResolver $resolver)
     {
         $resolver->setRequired(array('cache_dir', 'cache_refresh', 'cache_lifetime', 'web_url'
-            , 'web_dir', 'web_cache_dir', 'web_cache_url', 'less_import_dirs', 'less_variables'));
+            , 'web_dir', 'web_cache_dir', 'web_cache_url'));
 
         $resolver->setDefaults(array(
             'ignoring_files'    => array(),
             'less_import_dirs'  => array(),
-            'less_variables'     => array(),
+            'less_variables'    => array(),
+            'scss_import_dirs'  => array(),
+            'scss_variables'    => array(),
             'versioning_enable'    => false,
             'versioning_version'   => 1,
             'versioning_timestamp' => false,
@@ -82,7 +84,9 @@ class HtmlResourcesUtility
             'web_cache_dir'  => 'string',
             'web_cache_url'  => 'string',
             'less_import_dirs'  => 'array',
-            'less_variables'     => 'array',
+            'less_variables'    => 'array',
+            'scss_import_dirs'  => 'array',
+            'scss_variables'    => 'array',
             'versioning_enable'    => 'bool',
             'versioning_version'   => 'numeric',
             'versioning_timestamp' => 'bool',
@@ -137,6 +141,8 @@ class HtmlResourcesUtility
             if ($type == 'stylesheet') {
                 $combine->setLessImportDirs($options['less_import_dirs']);
                 $combine->setLessVariables($options['less_variables']);
+                $combine->setScssImportDirs($options['scss_import_dirs']);
+                $combine->setScssVariables($options['scss_variables']);
             }
 
             $res->setCombineObject($combine);
